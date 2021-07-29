84990fe76fccb84cbc407336206ce7102a45d478
twyair <yairt16@gmail.com>
2021-07-29 16:35:09 +0300

    organize md files in folders

--- generate-slides.js
+++ generate-slides.js
@@ -2,10 +2,10 @@ var fs = require('fs');
 var path = require('path');
 var Handlebars = require("handlebars");
 
-function buildHtml(output_dir, renderTemplate, name) {
+function buildHtml(output_dir, renderTemplate, name, dir) {
     console.log("generating slide for ", name);
     var html = renderTemplate({tutorial_name: name});
-    fs.writeFile(`${output_dir}/${name}.html`, html, err => {
+    fs.writeFile(`${output_dir}/${dir}-${name}.html`, html, err => {
         if (err) console.log(err);
         console.log("File written succesfully");
     });
@@ -15,8 +15,8 @@ const output_dir = path.join(__dirname, "./slides");
 const md_dir = path.join(__dirname, "./md");
 
 
-function generate_slides(template_name, prefix) {
-    fs.readdir(md_dir, (err, files) => {
+function generate_slides(template_name, dir) {
+    fs.readdir(path.join(md_dir, dir), (err, files) => {
         if (err) {
             console.error("Could not list the directory.", err);
             process.exit(1);
@@ -26,12 +26,12 @@ function generate_slides(template_name, prefix) {
         var renderTemplate = Handlebars.compile(template);
 
         files.forEach((file, _index) => {
-            if (file.endsWith(".md") && file.startsWith(prefix)) {
-                buildHtml(output_dir, renderTemplate, file.substring(0, file.length - 3));
+            if (file.endsWith(".md")) {
+                buildHtml(output_dir, renderTemplate, file.substring(0, file.length - 3), dir);
             }
         });
     });
 }
 
-generate_slides('ocaml-slide-template.hbs', 'ocaml-');
-generate_slides('prolog-slide-template.hbs', 'prolog-');
+generate_slides('ocaml-slide.hbs', 'ocaml');
+generate_slides('prolog-slide.hbs', 'prolog');
